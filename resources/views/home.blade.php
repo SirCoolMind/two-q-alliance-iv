1d10bdb28a952e454296d60cc897ef6ccd191d40
Hafiz Ruslan <hafizcoolman@gmail.com>
2025-03-14 03:20:49 +0800

    [Company] Add dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,29 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-end">
         <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Company Statistics') }}</div>
+
+                <div class="card-body">
+                    <div class="row justify-content-start">
+                        <div class="col-md-8 pb-3">
+                            <span>
+                                Total Company :
+                            </span>
+                        </div>
+                        <div class="col-md-8">
+                            <a href="#your-module-link" class="btn btn-primary">
+                                Go to this module <i class="bi bi-box-arrow-in-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
